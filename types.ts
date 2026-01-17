caaafef8f73c77632bac68bd2a7015396628bdc2
pro22334455 <brahim.medjadji.visa@gmail.com>
2026-01-17 10:07:23 +0100

    Add ChatMessage and VoiceActivity interfaces

--- types.ts
+++ types.ts
@@ -5,13 +5,30 @@ export interface User {
   avatar: string | null;
 }
 
+export interface ChatMessage {
+  sender: string;
+  text: string;
+  time: number;
+}
+
+export interface VoiceActivity {
+  p1?: boolean;
+  p2?: boolean;
+}
+
 export interface Room {
   id: string;
   creator: User;
-  timeLimit: number; // in minutes
+  timeLimit: number;
   createdAt: number;
-  status: 'waiting' | 'playing';
+  status: 'waiting' | 'playing' | 'closed';
   opponent?: User;
+  board?: DamaBoard;
+  turn?: 1 | 2;
+  p1Time?: number;
+  p2Time?: number;
+  chat?: Record<string, ChatMessage>;
+  voiceActivity?: VoiceActivity;
 }
 
 export interface DamaPiece {
